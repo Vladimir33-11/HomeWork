cfdd605211911798033f22f6bc9666a746d2b6b7
Vladimir <Vladimir33-11@yandex.ru>
2022-10-15 20:46:53 +0400

    HomeWork1-1

--- HomeWork1/Program.cs
+++ HomeWork1/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Задача 2 Программа на вход 2 числа, вывод максимального и минимального
+int a,b;
+string c,d;
+Console.WriteLine("Введите первое число: ");
+a=int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число: ");
+b=int.Parse(Console.ReadLine()!);
+if (a>b)
+{
+    Console.WriteLine($"max={a},min={b}");
+}
+else if(a==b)
+{
+    Console.WriteLine($"введенные числа равны");
+}
+else
+{
+    Console.WriteLine($"max={b},min={a}");
+}
