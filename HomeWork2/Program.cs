37f775fcbd5bdc45c4db0bd236ac6f3c5dbff499
Vladimir <Vladimir33-11@yandex.ru>
2022-10-15 21:33:57 +0400

    HomeWork2

---
+++ HomeWork2/Program.cs
@@ -0,0 +1,19 @@
+﻿//  Задача 2 Программа на вход 3 числа, вывод максимального 
+int a,b,c;
+
+Console.WriteLine("Введите первое число: ");
+a=int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число: ");
+b=int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите третье число: ");
+c=int.Parse(Console.ReadLine()!);
+int max = a;
+if (max<b)
+{
+   max = b;
+}
+if (max<c)
+{
+    max = c;
+}
+Console.WriteLine($"max={max}");
