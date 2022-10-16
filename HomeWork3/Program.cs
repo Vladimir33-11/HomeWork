76319919b08100da98ed3e35c0eddafdbafc7aff
Vladimir <Vladimir33-11@yandex.ru>
2022-10-16 10:17:40 +0400

    HomeWork3

---
+++ HomeWork3/Program.cs
@@ -0,0 +1,12 @@
+﻿//  программа на ввод число, вывод четное или нечетное
+int a;
+Console.WriteLine("Введите число: ");
+a=int.Parse(Console.ReadLine());
+if (a%2==0)
+{
+    Console.WriteLine("Четное! ");
+}
+else
+{
+    Console.WriteLine("Нечетное! ");
+}
