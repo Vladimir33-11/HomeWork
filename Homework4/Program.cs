e0cdb0161247f5b0070f47976959c9fca28ae58f
Vladimir <Vladimir33-11@yandex.ru>
2022-10-16 10:47:11 +0400

    Homework4

---
+++ Homework4/Program.cs
@@ -0,0 +1,12 @@
+﻿// программа ввод числа n; вывод все четные от 1 до n
+int n,i;
+Console.WriteLine("Введите число: ");
+//n = int.Parse(Console.ReadLine());
+int.TryParse(Console.ReadLine(),out n);
+i = 2;
+Console.WriteLine("Четные числа: ");
+while (i<=n)
+{
+    Console.Write($"{ i } ");
+    i=i + 2;
+}
