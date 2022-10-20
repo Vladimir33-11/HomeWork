5834339cc0bd0d5946531ac1e25d51b7a0011a5b
Vladimir <Vladimir33-11@yandex.ru>
2022-10-20 18:00:48 +0400

    add  Семинар 2 Задача 13

---
+++ Задача13/Program.cs
@@ -0,0 +1,22 @@
+﻿// ввод числa - вывод третья цифра числа или ее нет
+Console.WriteLine(" Введите число");
+int a, b, c, d, i;
+int.TryParse(Console.ReadLine(), out a);
+i = 100; 
+d= 1;
+if (a/100==0)
+{    Console.WriteLine($" третьей цифры нет" );
+     return;
+}
+while (a/i > 0)
+{
+i=i*10;
+d = d * 10;
+} 
+d=d/10;
+c=i/100;
+b=a%c;
+Console.WriteLine($"третья цифра {b/d}");
+
+
+
