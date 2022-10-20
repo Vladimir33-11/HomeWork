a9e261d5ea04fbc5f3a8a7e073a86350c0fb7b45
Vladimir <Vladimir33-11@yandex.ru>
2022-10-20 16:08:06 +0400

    add Семинар 2 задача 10

---
+++ Задача10/Program.cs
@@ -0,0 +1,6 @@
+﻿// ввод трехзначное число? вывод вторая цифра числа
+Console.WriteLine(" Введите трехзначное число");
+int a;
+int.TryParse(Console.ReadLine(), out a);
+Console.WriteLine($"{(a-a/100*100)/10}");
+
