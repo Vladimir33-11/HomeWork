e286608b9018c9b528510c904f211153b8903345
Vladimir <Vladimir33-11@yandex.ru>
2022-10-21 11:33:34 +0400

    Семинар 2 Задача 15

---
+++ Задача15/Program.cs
@@ -0,0 +1,20 @@
+﻿// на вхо цифра обозначающая день недели, 
+// выход является ли этот день выходным
+Console.WriteLine("Введите день недели в соответствии:");
+Console.WriteLine("1 -  понедельник");
+Console.WriteLine("2 -  вторник");
+Console.WriteLine("2 -  среда");
+Console.WriteLine("4 -  четверг");
+Console.WriteLine("5 -  пятница");
+Console.WriteLine("6 -  суббота");
+Console.WriteLine("7 -  воскресенье");
+int a;
+int.TryParse(Console.ReadLine(), out a);
+if (a == 6 || a == 7)
+{
+   Console.WriteLine("Выходной день"); 
+}
+else
+{
+       Console.WriteLine("Рабочий день");
+}
